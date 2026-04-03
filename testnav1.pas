743b8470c84ab391d03fdf9d2534791e6f12135a
deepproglearn <a34102733@gmail.com>
2026-04-03 22:11:52 +0300

    bug fixes,added smarter enter,cod is much cleaner,memory control is better, better optimization

--- testnav1.pas
+++ testnav1.pas
@@ -1,7 +1,11 @@
 program navigate;
 uses crt,sysutils;
 const
-	startline = 7;
+	startline = 2;
+	KEY_UP = #72;
+	KEY_DOWN = #80;
+	KEY_LEFT = #75;
+	KEY_RIGHT = #77;
 	rst = #27'[0m';
 	red1 = #27'[31m';
 	grn = #27'[32m';
@@ -18,23 +22,19 @@ type
 		len:integer;
 	end;
 	arstr = array of array of char;
-procedure writescreen(buffer:arstr;whereisx,whereisy,laststring:longint;insrt,syntaxhighlight:boolean;keywords:array of keyword); {every single symbol program will rewrite screen with buffer array}
+	zoom = array of integer;
+procedure writescreen(sizeofalllines:zoom;buffer:arstr;whereisx,whereisy,laststring:longint;insrt,syntaxhighlight:boolean;keywords:array of keyword); {every single symbol program will rewrite screen with buffer array}
 var
 	i,f,p,v:longint;
 	tmp:string;
 begin
 	clrscr;
-	writeln('       _ _   _            _              _ _ _ ');
-    	writeln('__   _(_|_) | |_ _____  _| |_    ___  __| (_) |_ ___  _ __ ');
-   	writeln('\ \ / / | | | __/ _ \ \/ / __|  / _ \/ _` | | __/ _ \| __|');
-    	writeln(' \ V /| | | | ||  __/>  <| |_  |  __/ (_| | | || (_) | |  ');
-   	writeln('  \_/ |_|_|  \__\___/_/\_\\__|  \___|\__,_|_|\__\___/|_|  ');
 	if insrt then
 		writeln('--INSERT--');
 	if not insrt then
 		writeln('--COMMAND--');
     	writeln;
-	if syntaxhighlight then
+	if syntaxhighlight and (pos('.pas',Paramstr(1)) > 0) then
 	begin
 		for i := 0 to high(buffer) do
 	begin
@@ -46,8 +46,11 @@ begin
 			for v := 1 to keyword_count do
 			begin
 				p := pos(keywords[v].wrd,tmp);
-				if p > 0 then
-					tmp := keywords[v].color + tmp + RST
+				if (p > 0) and (tmp[p+keywords[v].len+1] >= #32) and (((tmp[p-1] = #32) or (tmp[p-1] = #9)) or (p=1)) then
+				begin
+					insert(keywords[v].color,tmp,p);
+					tmp := tmp + RST
+				end;
 			end;
 		end;
 		if syntaxhighlight then
@@ -62,7 +65,7 @@ begin
 		write(RST);
 	end;
 	end;
-	if not syntaxhighlight then
+	if not syntaxhighlight or (syntaxhighlight and not(pos('.pas',Paramstr(1)) > 0)) then
 	begin
 		for i := 0 to high(buffer) do
 		begin
@@ -71,17 +74,17 @@ begin
 				write(buffer[i] [f])
 		end;
 	end;
-	gotoXY(whereisx+1,whereisy)	
+	gotoXY(whereisx,whereisy)	
 end;
 var
 	buffer:array of array of char;
 	sizeofalllines: array of integer;
-	f1,config:text;
+	f1,config,inpt:text;
 	x:string;
 	c,g,temp,temp1:char;
-	i,ic,v,f,p:integer;
-	alllines,laststr,num,countsoc,whereisx,whereisy,laststring:longint;
-	up,insrt,arrow,syntaxhighlight,cursorjump,autoident:boolean;
+	i,ic,v,f,p,z:integer;
+	alllines,laststr,num,countsoc,whereisx,whereisy,laststring,currentbuffer,lastbuffer,zeb,limitcurbuffer,limitlsbuffer,y,realsize:longint;
+	up,insrt,arrow,syntaxhighlight,cursorjump,autoident,memallocated:boolean;
 	keywords:array [1..keyword_count] of keyword = (
 		(wrd: 'begin';color: ylw;len:5),
 		(wrd: 'end';color:ylw;len:3),
@@ -98,14 +101,9 @@ var
 
 begin
 	clrscr;
-	writeln('       _ _   _            _              _ _ _ '); {first enter to the program}
-    	writeln('__   _(_|_) | |_ _____  _| |_    ___  __| (_) |_ ___  _ __ ');
-   	writeln('\ \ / / | | | __/ _ \ \/ / __|  / _ \/ _` | | __/ _ \| __|');
-    	writeln(' \ V /| | | | ||  __/>  <| |_  |  __/ (_| | | || (_) | |  ');
-   	writeln('  \_/ |_|_|  \__\___/_/\_\\__|  \___|\__,_|_|\__\___/|_|  ');
-    	writeln;
 	insrt := false; {intializing variables}
 	ic := 0;
+	y := 0;
 	cursorjump := false;
 	syntaxhighlight := false;
 	cursorjump := false;
@@ -146,46 +144,106 @@ begin
 		end;
 		close(config);
 	end;
+	z := 0;
 	alllines := 1;
 	whereisy := startline;
 	whereisx := 1;
 	countsoc := 1;
-	setlength(buffer,1);
-	setlength(buffer[0],1);
-	buffer[high(buffer)] [high(buffer[high(buffer)])] := #0;
-	setlength(sizeofalllines,1);
+	currentbuffer := 1;
+	limitcurbuffer := 10;
+	limitlsbuffer := 5;
+	lastbuffer := 1;
+	setlength(buffer,limitcurbuffer);
+	setlength(buffer[0],limitlsbuffer);
+	buffer[0] [0]:= #0;
+	laststring := 1;
+	setlength(sizeofalllines,limitcurbuffer);
+	if FileExists(Paramstr(1)) then
+	begin
+		assign(inpt,Paramstr(1));
+		reset(inpt);
+		while not eof(inpt) do
+		begin
+			read(inpt,g);
+			if alllines >= limitcurbuffer then
+			begin
+				limitcurbuffer := limitcurbuffer * 2;
+				setlength(buffer,limitcurbuffer);
+				setlength(sizeofalllines,limitcurbuffer);
+			end;
+			if g = #9 then
+			begin
+				if z >= high(buffer[alllines-1]) then
+					setlength(buffer[alllines-1],length(buffer[alllines-1]) * 2);
+				lastbuffer := lastbuffer + 1;
+				buffer[alllines-1] [z] := #9;
+				z := z + 1;
+				sizeofalllines[alllines-1] := sizeofalllines[alllines-1] + 1;
+			end;
+			if g = #10 then
+			begin
+				alllines := alllines + 1;
+				currentbuffer := currentbuffer + 1;
+				lastbuffer := lastbuffer + 1;
+				countsoc := 0;
+				laststring := laststring + 1;
+				z := 0;
+				setlength(buffer[alllines-1],limitlsbuffer);
+			end;
+			if (g<>#0) and (g >= #32) then
+			begin
+				if z >= high(buffer[alllines-1]) then
+					setlength(buffer[alllines-1],length(buffer[alllines-1]) * 2);
+				lastbuffer := lastbuffer + 1;
+				sizeofalllines[alllines-1] := sizeofalllines[alllines-1] + 1;
+				countsoc := countsoc + 1;
+				buffer[alllines-1] [z] := g;
+				lastbuffer := lastbuffer + 1;
+				z := z + 1;
+			end;
+		end;
+		close(inpt);
+	end;
 	up := false;
 	sizeofalllines[0] := 0;
 		insrt := false;
 	while true do {main program}
 	begin
-		writescreen(buffer,whereisx,whereisy,laststring,insrt,syntaxhighlight,keywords); {rewriting screen every iteration}
+		if round(currentbuffer*1.5) > limitcurbuffer then
+		begin
+			limitcurbuffer := limitcurbuffer * 2;
+			setlength(buffer,limitcurbuffer);
+			setlength(sizeofalllines,limitcurbuffer);
+		end;
+		while buffer[whereisy-startline] [y] > #0 do
+			y := y + 1;
+		writescreen(sizeofalllines,buffer,whereisx,whereisy,laststring,insrt,syntaxhighlight,keywords); {rewriting screen every iteration}
 		arrow := false;
 		c := readkey;
 		if c = #0 then
 		begin
 			c := readkey;
-			if (c = #72) and (whereisy <> 1) then
+			if (c = KEY_UP) and (whereisy-startline >= 1) then
 			begin
-				if ((whereisx > sizeofalllines[whereisy-startline-1]) or (whereisx = sizeofalllines[whereisy-startline]+1)) and (cursorjump) then
-					whereisx := sizeofalllines[whereisy-startline-1] + 1;
+				if ((whereisx-1 > high(buffer[whereisy-startline-1])) or (whereisx-1 = high(buffer[whereisy-startline])+1)) and (cursorjump) then
+					whereisx := length(buffer[whereisy-startline-1]) + 1;
 				whereisy := whereisy - 1;
 			end;
-			if (c = #80) then
+			if (c = KEY_DOWN) then
 			begin
 				if whereisy-startline < alllines then
 					whereisy := whereisy + 1;
-				if ((whereisx > sizeofalllines[whereisy-startline-1]) or (whereisx = sizeofalllines[whereisy-startline]+1)) and (cursorjump) then
-					whereisx := sizeofalllines[whereisy-startline-1] + 1;
+				if ((whereisx > length(buffer[whereisy-startline-1])) or (whereisx = length(buffer[whereisy-startline])+1)) and (cursorjump) then
+					whereisx := length(buffer[whereisy-startline-1]) + 1;
 			end;
-			if (c = #75) then
+			if (c = KEY_LEFT) then
 			begin
 				if whereisx > 1 then
 					whereisx := whereisx - 1;
 			end;
-			if (c = #77) then
+			if (c = KEY_RIGHT) then
 			begin
-				if whereisx <= sizeofalllines[whereisy - startline] then
+				if whereisx <= y then
 					whereisx := whereisx + 1;
 			end;
 			arrow := true;
@@ -204,14 +262,16 @@ begin
 			ic := ic + 1;
 			if (c > #0) and (ic > 1) and (c <> #8) and (c>=#32)then
 			begin
-				setlength(buffer[whereisy-startline], length(buffer[whereisy-startline])+1);
+				lastbuffer := lastbuffer + 1;
 				sizeofalllines[whereisy-startline] := sizeofalllines[whereisy-startline] + 1;
 				whereisx := whereisx + 1;
 				countsoc := countsoc + 1;
-				if whereisx-1 <> sizeofalllines[whereisy-startline] then
+				if whereisx > high(buffer[whereisy-startline]) then
+					setlength(buffer[whereisy-startline],length(buffer[whereisy-startline]) * 2);
+				if whereisx-1 <> length(buffer[whereisy-startline]) then
 				begin
-					setlength(buffer[whereisy-startline],length(buffer[whereisy-startline])+1);
-					for i := sizeofalllines[whereisy-startline] downto whereisx-1 do
+					lastbuffer := lastbuffer + 1;
+					for i := high(buffer[whereisy-startline]) downto whereisx-1 do
 					begin
 						buffer[whereisy-startline] [i+1] := buffer[whereisy-startline] [i];
 					end;
@@ -222,41 +282,40 @@ begin
 			begin
 				if whereisx > 1 then
 				begin
-					if whereisx-1 < sizeofalllines[whereisy - startline] then
-					begin
-						for i := whereisx-1 to sizeofalllines[whereisy-startline]-1 do
-							buffer[whereisy-startline] [i] := buffer[whereisy-startline] [i+1]
-					end;
-					setlength(buffer[whereisy-startline],length(buffer[whereisy-startline]) - 1);
+					buffer[whereisy-startline] [whereisx-1] := #0;
+					lastbuffer := lastbuffer - 1;
 					countsoc := countsoc - 1;
 					whereisx := whereisx - 1;
 					sizeofalllines[whereisy-startline] := sizeofalllines[whereisy-startline] - 1
-				end;
+				end;			
 			end;
 			if c = #9 then
 			begin
-				setlength(buffer[high(buffer)], length(buffer[high(buffer)]) + 1);
+				lastbuffer := lastbuffer + 1;
 				buffer[whereisy-startline] [whereisx-1]:= #9
 			end;
 			if c = #13 then
 			begin
-
-				setlength(sizeofalllines,alllines);
-				if whereisy-startline < alllines then
-				begin
-					setlength(sizeofalllines,length(sizeofalllines) + 1);
-					for i := whereisy-startline downto alllines do
-					begin
-						sizeofalllines[i+1] := sizeofalllines[i]
-					end;
-				end;
 				alllines := alllines + 1;
-				setlength(buffer, alllines);
-				setlength(buffer[alllines-1], 1);
+				currentbuffer := currentbuffer + 1;
+				lastbuffer := lastbuffer + 1;
 				whereisy := whereisy + 1;				
 				whereisx := 1;
 				countsoc := 0;
 				laststring := laststring + 1;
+				setlength(buffer[alllines-1],limitlsbuffer);
+				if whereisy-startline+1 < alllines then
+				begin
+					zeb := 0;
+					for i := alllines-1 downto whereisy-startline	do
+					begin
+						buffer[i] := buffer[i-1];
+						sizeofalllines[i] := sizeofalllines[i-1]
+					end;
+					setlength(buffer[whereisy-startline],limitlsbuffer);
+					for i := 0 to limitlsbuffer-1 do
+						buffer[whereisy-startline] [i] := #0;
+				end;				
 			end;
 			if c = #27 then
 			begin
@@ -272,9 +331,9 @@ begin
 	begin
 		assign(f1,Paramstr(1));
 		rewrite(f1);
-		for f := 0 to high(buffer) do
+		for f := 0 to alllines-1 do
 		begin
-			for v := 0 to high(buffer[f]) do
+			for v := 0 to length(buffer[f]) do
 			begin
 				if buffer[f][v] <> #0 then
 					write(f1,buffer[f][v]);
